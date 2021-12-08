9f011b6d9d6798b2afe14acc7a5150c43cc9e336
= <jaime.polidura@gmail.com>
2021-12-08 20:42:12 +0100

    added new rabbitmq structure configuration

--- gateway/src/main/java/es/jaime/gateway/_shared/infrastrocture/rabbitmq/RabbitMQConfiguration.java
+++ gateway/src/main/java/es/jaime/gateway/_shared/infrastrocture/rabbitmq/RabbitMQConfiguration.java
@@ -1,14 +1,18 @@
 package es.jaime.gateway._shared.infrastrocture.rabbitmq;
 
 import es.jaime.gateway.listedcompanies._shared.domain.ListedCompaniesRepository;
+import es.jaime.gateway.listedcompanies._shared.domain.ListedCompany;
 import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class RabbitMQConfiguration {
     public static final String directExchangeName = "sxs.new-orders";
-    public static final String newOrders = "sxs.new-orders";
     private final ListedCompaniesRepository repository;
 
     public RabbitMQConfiguration(ListedCompaniesRepository repository) {
@@ -17,23 +21,27 @@ public class RabbitMQConfiguration {
 
     @Bean
     public Declarables declarables() {
+        List<Declarable> declarables = new ArrayList<>();
+
         DirectExchange directExchange = new DirectExchange(directExchangeName);
+        declarables.add(directExchange);
 
-        return new Declarables(directExchange);
+        List<Queue> allQueuesForListedCompanies = repository.findAll().stream()
+                .map(listedCompany -> listedCompany.ticker().value())
+                .map(listedCompabyTicker -> directExchangeName + "." + listedCompabyTicker)
+                .map(queueFullNmeString -> new Queue(queueFullNmeString, false))
+                .collect(Collectors.toList());
+
+        List<Binding> allBindings = allQueuesForListedCompanies.stream()
+                .map(queue -> BindingBuilder
+                        .bind(queue)
+                        .to(directExchange)
+                        .with(queue.getName()))
+                .collect(Collectors.toList());
+
+        declarables.addAll(allBindings);
+        declarables.addAll(allQueuesForListedCompanies);
+
+        return new Declarables(declarables);
     }
-
-//    @Bean
-//    public Queue queue() {
-//        return new Queue(newOrders, false);
-//    }
-//
-//    @Bean
-//    public TopicExchange exchange() {
-//        return new TopicExchange(directExchangeName);
-//    }
-//
-//    @Bean
-//    public Binding binding(Queue queue, TopicExchange exchange) {
-//        return BindingBuilder.bind(queue).to(exchange).with("sxs.new-orders");
-//    }
 }
