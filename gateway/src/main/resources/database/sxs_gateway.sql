891533deb6f3ded53573b5f6ce1f089d81e63842
= <jaime.polidura@gmail.com>
2021-12-10 23:21:28 +0100

    changed mysql database setup file

--- gateway/src/main/resources/database/sxs_gateway.sql
+++ gateway/src/main/resources/database/sxs_gateway.sql
@@ -1,62 +1,51 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : MySQL
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : sxs_gateway
-
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
-
-Date: 2021-11-20 18:12:16
-*/
+/*Source Database       : sxs_gateway*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for activeorders
--- ----------------------------
 CREATE TABLE IF NOT EXISTS `activeorders` (
-  `activeorderId` varchar(36) NOT NULL,
-  `clientId` varchar(36) NOT NULL,
-  `date` varchar(15) NOT NULL,
-  `quantity` int(10) NOT NULL,
-  `type` varchar(4) NOT NULL,
-  `executionType` varchar(6) NOT NULL,
-  `executionPrice` double(10,3) NOT NULL,
-  PRIMARY KEY (`activeorderId`)
+    `activeorderId` varchar(50) NOT NULL,
+    `ticker` varchar(10) NOT NULL,
+    `clientId` varchar(36) NOT NULL,
+    `date` varchar(30) NOT NULL,
+    `quantity` int(10) NOT NULL,
+    `type` varchar(4) NOT NULL,
+    `executionPrice` double(10,3) NOT NULL,
+    `status` varchar(10) NOT NULL,
+    PRIMARY KEY (`activeorderId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of activeorders
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for listedcompanies
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `listedcompanies` (
-  `ticker` varchar(10) NOT NULL,
-  PRIMARY KEY (`ticker`),
-  UNIQUE KEY `ticker` (`ticker`) USING HASH
+    `ticker` varchar(10) NOT NULL,
+    `name` varchar(30) NOT NULL,
+    `currencyCode` varchar(10) NOT NULL,
+    `currencySymbol` varchar(5) NOT NULL,
+    PRIMARY KEY (`ticker`),
+    UNIQUE KEY `ticker` (`ticker`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of listedcompanies
--- ----------------------------
-# INSERT INTO `listedcompanies` VALUES ('ADBE');
-# INSERT INTO `listedcompanies` VALUES ('AMZN');
-# INSERT INTO `listedcompanies` VALUES ('BAC');
-# INSERT INTO `listedcompanies` VALUES ('BRK.A');
-# INSERT INTO `listedcompanies` VALUES ('FB');
-# INSERT INTO `listedcompanies` VALUES ('GOOG');
-# INSERT INTO `listedcompanies` VALUES ('HD');
-# INSERT INTO `listedcompanies` VALUES ('JNJ');
-# INSERT INTO `listedcompanies` VALUES ('JPM');
-# INSERT INTO `listedcompanies` VALUES ('MA');
-# INSERT INTO `listedcompanies` VALUES ('MSFT');
-# INSERT INTO `listedcompanies` VALUES ('NVDA');
-# INSERT INTO `listedcompanies` VALUES ('PG');
-# INSERT INTO `listedcompanies` VALUES ('TSLA');
-# INSERT INTO `listedcompanies` VALUES ('V');
+
+DELETE FROM listedcompanies;
+
+INSERT INTO `listedcompanies` VALUES ('ADBE', 'Adobe', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('AMZN', 'Amazon', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('BAC', 'Bank of America', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('BRK.A', 'Berkshire Hathaway', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('DGV', 'Digital value', 'EUR', '€');
+INSERT INTO `listedcompanies` VALUES ('DND', 'Dye & Durham', 'CAD', 'CAD');
+INSERT INTO `listedcompanies` VALUES ('FB', 'Facebook', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('GOOG', 'Google', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('HD', 'Home Depot', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('JNJ', 'Johnson & Johnson', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('JPM', 'JP Morgan', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('LMT', 'Lockheed Martin', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('MA', 'Mastercard', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('MSFT', 'Microsoft', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('MTY', 'MTY Food', 'CAD', 'CAD');
+INSERT INTO `listedcompanies` VALUES ('NVDA', 'Nvidia', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('PG', 'Procter & Gamble', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('TSLA', 'Tesla', 'USD', '$');
+INSERT INTO `listedcompanies` VALUES ('V', 'Visa', 'USD', '$');
+
