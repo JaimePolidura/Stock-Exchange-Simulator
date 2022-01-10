7d860cdc35533a0699fdb363b09e2cb37516afc9
JaimeTruman <jaime.polidura@gmail.com>
2022-01-10 17:55:23 +0100

    added typescript to client-order-event-dispatcher

---
+++ client-order-event-dispatcher/index.ts
@@ -0,0 +1,68 @@
+const axios = require('axios').default;
+const express = require("express");
+const app = express();
+const http = require('http');
+const ampq = require('amqplib/callback_api');
+
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+
+const queuesListener: string[] = [
+    "sxs.exchange.events.client-order-event-dispatcher.order-error",
+    "sxs.exchange.events.client-order-event-dispatcher.order-executed-buy",
+    "sxs.exchange.events.client-order-event-dispatcher.order-executed-sell",
+    "sxs.exchange.events.client-order-event-dispatcher.order-cancelled",
+];
+
+const gateway: string = "http://localhost:8080/";
+
+const io = socketIo(server, {
+    origin: "*",
+});
+
+server.listen(4000, () => {
+    console.log("Server listening on port 4000");
+});
+
+io.on('connection', socket => {
+    console.log("connected id: " + socket.id);
+});
+
+io.use((socket, next) => {
+    const username: string = socket.handshake.auth.username;
+    const token: string = socket.handshake.auth.token;
+
+    authenticate(socket, username, token);
+
+    next();
+});
+
+const authenticate = (socket, username: string, token: string): void => {
+    axios.get(`${gateway}auth/isvalidtoken?username=${username}&token=${token}`)
+        .then(res => {if (res.data == false) {closeSocketConnection(socket)}})
+        .catch(err => closeSocketConnection(socket));
+}
+
+const closeSocketConnection = (socket): void => socket.disconnect(0);
+
+ampq.connect('amqp://rabbitmq', (errorConnection, connection) => {
+    if (errorConnection) throw errorConnection;
+
+    console.log("connected to rabbitmq");
+
+    connection.createChannel((errorChannel, channel) => {
+        if (errorChannel) throw errorChannel;
+
+        queuesListener.forEach(queue => {
+            channel.consume(queue, message => {
+                let messageToJSON: any = JSON.parse(message.content.toString());
+
+                console.log("new message from exchange "+ messageToJSON.to  +": " + message.content.toString());
+
+                messageToJSON.meta.to.forEach(to => {
+                    io.emit(to, messageToJSON);
+                });
+            });
+        });
+    });
+});
